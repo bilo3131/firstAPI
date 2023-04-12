b220e892d2c7bcc2ec946cd2b1a812f109765162
Bilal Alac <bilalalac@gmail.com>
2023-04-12 07:14:40 +0200

    first child element styled

--- script.js
+++ script.js
@@ -1,38 +1,59 @@
 let blocks = [
     {
-        'start': 0,
-        'end': 30
+        'start': 1,
+        'end': 26
     },
     {
-        'start': 30,
-        'end': 60
+        'start': 26,
+        'end': 51
     },
     {
-        'start': 60,
-        'end': 90
+        'start': 51,
+        'end': 76
     },
     {
-        'start': 90,
-        'end': 120
+        'start': 76,
+        'end': 101
     },
     {
-        'start': 120,
-        'end': 151
+        'start': 101,
+        'end': 126
+    },
+    {
+        'start': 126,
+        'end': 152
     }
 ]
 
 let counter = 0;
 let currentPokemon;
+let allPokemon = [];
+let blockStart = blocks[counter]['start'];
+let blockEnd = blocks[counter]['end'];
 
 async function loadPokemon() {
-    let urlNames = 'https://pokeapi.co/api/v2/pokemon/bulbasaur';
+    // for (let i = blockStart; i < blockEnd; i++) {
+        loadFromNet();
+    // }
+    
+    
+    
+}
+
+async function loadFromNet() {
+    let urlNames = `https://pokeapi.co/api/v2/pokemon/1`;
     let responseNames = await fetch(urlNames);
     currentPokemon = await responseNames.json();
+    allPokemon.push(currentPokemon['name'])
     console.log(currentPokemon);
 
-    // renderPokemon();
+    renderPokemon();
 }
 
 function renderPokemon() {
-
+    document.getElementById('pokemonImage').src = currentPokemon['sprites']['other']['dream_world']['front_default'];
+    document.getElementById('pokemon').classList.add(currentPokemon['types']['0']['type']['name'])
+    document.getElementById('pokemonName').innerHTML = currentPokemon['name'].toUpperCase();
+    document.getElementById('type1').innerHTML = (currentPokemon['types']['0']['type']['name']).toUpperCase();
+    document.getElementById('type2').innerHTML = (currentPokemon['types']['1']['type']['name']).toUpperCase();
 }

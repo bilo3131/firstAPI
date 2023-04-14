0ea2574714e1c24627163170f97223f0c9393abb
Bilal Alac <bilalalac@gmail.com>
2023-04-14 03:56:36 +0200

    main completed

--- script.js
+++ script.js
@@ -25,35 +25,108 @@ let blocks = [
     }
 ]
 
+
+
 let counter = 0;
 let currentPokemon;
 let allPokemon = [];
-let blockStart = blocks[counter]['start'];
-let blockEnd = blocks[counter]['end'];
+
 
 async function loadPokemon() {
-    // for (let i = blockStart; i < blockEnd; i++) {
-        loadFromNet();
-    // }
-    
-    
-    
+    let blockStart = blocks[counter]['start'];
+    let blockEnd = blocks[counter]['end'];
+    for (let i = blockStart; i < blockEnd; i++) {
+        await loadFromNet(i);
+    }
 }
 
-async function loadFromNet() {
-    let urlNames = `https://pokeapi.co/api/v2/pokemon/1`;
+async function loadFromNet(i) {
+    let urlNames = `https://pokeapi.co/api/v2/pokemon/${i}`;
     let responseNames = await fetch(urlNames);
     currentPokemon = await responseNames.json();
-    allPokemon.push(currentPokemon['name'])
-    console.log(currentPokemon);
-
-    renderPokemon();
+    allPokemon.push(currentPokemon);
+    // console.log(allPokemon);
+    pokeInfo(currentPokemon);
+    input();
 }
 
-function renderPokemon() {
-    document.getElementById('pokemonImage').src = currentPokemon['sprites']['other']['dream_world']['front_default'];
-    document.getElementById('pokemon').classList.add(currentPokemon['types']['0']['type']['name'])
-    document.getElementById('pokemonName').innerHTML = currentPokemon['name'].toUpperCase();
-    document.getElementById('type1').innerHTML = (currentPokemon['types']['0']['type']['name']).toUpperCase();
-    document.getElementById('type2').innerHTML = (currentPokemon['types']['1']['type']['name']).toUpperCase();
+function pokeInfo(loaded) {
+    // let id = loaded['id'];
+    let name = loaded['name'].toUpperCase();
+    let typeOne = loaded['types']['0']['type']['name'];
+    let typeTwo;
+    let typesLength = loaded['types'].length;
+    if (typesLength > 1) {
+        typeTwo = loaded['types']['1']['type']['name'];
+    } else {
+        typeTwo = '';
+    }
+    let img = loaded['sprites']['other']['dream_world']['front_default'];
+    renderPokemon(name, typeOne, typeTwo, img);
+}
+
+function renderPokemon(name, typeOne, typeTwo, img) {
+    let pokedex = document.getElementById('pokedex');
+    pokedex.innerHTML += pokedexHTML(name, typeOne.toUpperCase(), typeTwo.toUpperCase(), img);
+    let pokemon = document.getElementById(name);
+    pokemon.classList.add(typeOne);
+    
+}
+
+function pokedexHTML(name, typeOne, typeTwo, img) {
+    return /*html*/ `
+        <div id="${name}" class="pokemon">
+            <div class="flexing">
+                <h2>${name}</h2>
+                <div>
+                    <h4>${typeOne}</h4>
+                    <h4>${typeTwo}</h4>
+                </div>
+            </div>
+            <img src="${img}">
+        </div>
+    `;
+}
+
+function loadNext() {
+    if (counter < 4) {
+        counter++;
+        loadPokemon();
+    } else if (counter == 4) {
+        counter++;
+        hideButton();
+        loadPokemon();
+    }
+}
+
+function hideButton() {
+    document.getElementById('hide').classList.add('d-none');
+}
+
+function input() {
+    let search = document.getElementById('search').value;
+    search = search.toUpperCase();
+    // console.log(currentPokemon);
+    document.getElementById('pokedex').innerHTML ='';
+    for (let i = 0; i < allPokemon.length; i++) {
+        const pokemon = allPokemon[i];
+        if (pokemon['name'].toUpperCase().includes(search)) {
+            pokeInfo(pokemon);
+        }
+    }
+}
+
+function selectedType(type) {
+    document.getElementById('pokedex').innerHTML ='';
+    for (let i = 0; i < allPokemon.length; i++) {
+        const pokemon = allPokemon[i];
+        if (pokemon['types']['0']['type']['name'].includes(type)) {
+            pokeInfo(pokemon);
+        }else if (pokemon['types'].length > 1 && pokemon['types']['1']['type']['name'].includes(type)) {
+            pokeInfo(pokemon);
+        } else if (type == 'all') {
+            pokeInfo(pokemon);
+            
+        }
+    }
 }
